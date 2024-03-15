01c664248de12daff94ed001b3632efa7a1e4d0d
AlejoPluma02 <fernandezalejo937@gmail.com>
2024-03-14 21:18:39 -0500

    El 3

---
+++ Punto 3/Punto 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Punto_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el radio (r): ");
+            double radio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el ángulo en grados: ");
+            double grados = Convert.ToDouble(Console.ReadLine());
+
+            double x, y;
+            ConvertirCoordenadas(radio, grados, out x, out y);
+
+            Console.WriteLine("Las coordenadas rectangulares son:");
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+
+            Console.ReadLine();
+        }
+
+        static void ConvertirCoordenadas(double radio, double grados, out double x, out double y)
+        {
+            double angulosrAdianes = GradosARadianes(grados);
+
+            x = radio * Math.Cos(angulosrAdianes);
+            y = radio * Math.Sin(angulosrAdianes);
+        }
+
+        static double GradosARadianes(double grados)
+        {
+            return grados * Math.PI / 180.0;
+        }
+    }
+}
